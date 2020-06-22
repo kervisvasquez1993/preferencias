6123c2dff1130f6942183f79c6a7548e54c16dbf
kervisvasquez1993 <kervisvasquez24@gmail.com>
2020-06-22 09:00:10 -0400

    agregamos commit de preferencias

--- lib/src/page/SettingsPage.dart
+++ lib/src/page/SettingsPage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:preferencias_git/src/wigdget/menuWidget.dart';
-class SettingsPage extends StatelessWidget {
+class SettingsPage extends StatelessWidget 
+{
 
   static final String routeName = 'settings';
   @override
@@ -11,8 +12,48 @@ class SettingsPage extends StatelessWidget {
       ),
       drawer: Menu(),
       
-      body: Center(
-        child : Text('ajustes'),
+      body: ListView(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.all(5.0),
+            child: Text('Settings', style: TextStyle( fontSize: 45.0, fontWeight: FontWeight.bold))
+            ),
+            Divider(),
+            SwitchListTile
+            (
+                value: true,
+                activeTrackColor: Colors.red,
+                inactiveThumbColor: Colors.green,
+                title: Text('Color Secundario'),
+                onChanged: (value){},
+            ),
+            RadioListTile(
+              value: 1,
+              title: Text('Masculino'),
+              groupValue: 1,
+              onChanged: (value){},
+            ),
+            RadioListTile(
+              value: 1,
+              title: Text('Femenino'),
+              groupValue: 2,
+              onChanged: (value){},
+            ),
+            Divider(),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 20.0), 
+              child: TextField(
+                decoration: InputDecoration(
+                  labelText: 'Nombre',
+                  helperText: 'Nombre de la persona usado'                  
+                ),
+                onChanged: (value){
+
+                },
+              ) ,
+            )
+        ],
+
       )
       
     );
