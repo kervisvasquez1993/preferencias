ecb648925726e5a2118277c68591a520c346cb9d
kervisvasquez1993 <kervisvasquez24@gmail.com>
2020-06-22 10:26:04 -0400

    seccion de ajuste finalizada

--- lib/src/page/SettingsPage.dart
+++ lib/src/page/SettingsPage.dart
@@ -1,9 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:preferencias_git/src/wigdget/menuWidget.dart';
-class SettingsPage extends StatelessWidget 
+class SettingsPage extends StatefulWidget 
 {
 
   static final String routeName = 'settings';
+
+  @override
+  _SettingsPageState createState() => _SettingsPageState();
+
+
+}
+class _SettingsPageState extends State<SettingsPage> {
+  bool _colorSecundario = false;
+  int _genero = 1;
+  String _nombre = 'kervis';
+
+  TextEditingController _textController ;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _textController  = new TextEditingController(text: _nombre);
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,28 +39,42 @@ class SettingsPage extends StatelessWidget
             Divider(),
             SwitchListTile
             (
-                value: true,
+                value: _colorSecundario,
                 activeTrackColor: Colors.red,
                 inactiveThumbColor: Colors.green,
                 title: Text('Color Secundario'),
-                onChanged: (value){},
+                onChanged: (value){
+                  
+                  setState(() {
+                    _colorSecundario = value;
+                  });
+                },
             ),
             RadioListTile(
               value: 1,
               title: Text('Masculino'),
-              groupValue: 1,
-              onChanged: (value){},
+              groupValue: _genero,
+              onChanged: (value){
+                setState(() {
+                  _genero = value;
+                });
+              },
             ),
             RadioListTile(
-              value: 1,
+              value: 2,
               title: Text('Femenino'),
-              groupValue: 2,
-              onChanged: (value){},
+              groupValue: _genero,
+              onChanged: (value){
+                setState(() {
+                  _genero = value;
+                });
+              },
             ),
             Divider(),
             Container(
               padding: EdgeInsets.symmetric(horizontal: 20.0), 
               child: TextField(
+                controller: _textController,
                 decoration: InputDecoration(
                   labelText: 'Nombre',
                   helperText: 'Nombre de la persona usado'                  
